e1b837d2cb3566b35973187050c6602ba3c3cdd2
tashipandit <93296077+tashipandit@users.noreply.github.com>
2022-04-02 11:36:17 -0400

    Add files via upload

--- letschat.js
+++ letschat.js
@@ -3,3 +3,26 @@ user_name= document.getElementById("user_name").value;
 localStorage.setItem("user_name",user_name);
 window.location="letschat_room.html";
 }
+    function addRoom()
+    {
+    room_name=document.getElementById("room_name").value;
+    firebase.database().ref("/").child(room_name).update({
+      purpose: "adding room name"
+    });
+    localStorage.setItem("room_name",room_name);
+    window.location="letschat_room.html";
+    }function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="letschat.html";
+      }
+      function send()
+      {
+      msg=document.getElementBYId("msg").value;
+      firebase.database().ref(room_names).push({
+            name:user_name,
+            message:msg,
+            like:0
+      });
+      document.getElementById("msg").value="";
+      }
--- letschat_room.js
+++ letschat_room.js
@@ -1,4 +1,12 @@
-
+const firebaseConfig = {
+      apiKey: "AIzaSyDq4qm8QxMd3cUuCUj9nkmkCy0t1AwDN38",
+      authDomain: "kwitter-5b599.firebaseapp.com",
+      databaseURL: "https://kwitter-5b599-default-rtdb.firebaseio.com",
+      projectId: "kwitter-5b599",
+      storageBucket: "kwitter-5b599.appspot.com",
+      messagingSenderId: "204766044886",
+      appId: "1:204766044886:web:d8f96c13a2f537d3b7df77"
+    };
 //ADD YOUR FIREBASE LINKS HERE
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
@@ -8,3 +16,17 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End code
       });});}
 getData();
+function addUser() {
+      user_name= document.getElementById("user_name").value;
+      localStorage.setItem("user_name",user_name);
+      window.location="kwitter_room.html";
+      }
+      function addRoom()
+      {
+      room_name=document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+        purpose: "adding room name"
+      });
+      localStorage.setItem("room_name",room_name);
+      window.location="kwitter_page.html";
+      }
